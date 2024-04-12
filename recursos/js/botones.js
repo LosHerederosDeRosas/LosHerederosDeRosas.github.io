8838d2af5365cdd375833d62628650c3065d52ff
LosHerederosDeRosas <LosHerederosDeRosas@gmail.com>
2024-04-11 21:01:43 -0300

    primer formato

---
+++ recursos/js/botones.js
@@ -0,0 +1,12 @@
+function cambiarFondo(){
+    var elemento=document.querySelector("#nave");
+    elemento.style.background="grey";
+}
+function cambiarBtn(){
+    var elemento=document.querySelector("#btn1");
+    elemento.innerHTML="Botón cambiado";
+    elemento.style.background="lightblue";
+}
+function ejecuta(){
+    alert("Has apretado el botón!!!");
+}
