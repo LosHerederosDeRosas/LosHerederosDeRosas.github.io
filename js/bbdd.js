017fa26e222c91bbe0316cfb3b6ecb9554be489f
LosHerederosDeRosas <LosHerederosDeRosas@gmail.com>
2024-04-16 12:03:50 -0300

    base_datos

--- js/bbdd.js
+++ js/bbdd.js
@@ -1,18 +1,63 @@
 
-const articulo1 =`
+
+const articulo =`
 {
-	"articulo":{
-		"autor":"Marco Debiasi",
-		"fecha":"15/04/2024",
+	"cristiandad":{
+		"id":0,
+		"autor":"Marco Patrico Debiasi",
+		"fecha":"16/04/2024",
+		"titulo":"Cristiandad",
 		"texto":[
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et semper ligula. Nullam lacinia libero quis felis molestie, nec faucibus libero consectetur. Quisque gravida urna vel nunc rutrum, vitae interdum turpis mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi accumsan lorem vitae bibendum dictum. Phasellus sollicitudin turpis et nisi sollicitudin, a dapibus dolor cursus. Ut et libero sed felis tincidunt efficitur. Phasellus commodo velit quis justo posuere, sed lacinia felis volutpat. Vivamus quis sodales metus. Sed luctus euismod tincidunt.<br><br>",
-			"Duis ac metus nec neque tincidunt vulputate. Donec eu ex nec ligula vulputate fermentum eget sit amet est. Vivamus at ante vel elit consectetur dapibus. Ut auctor lacus et diam laoreet, at tempor ipsum volutpat. Nullam ut nisi purus. Cras non bibendum arcu, at auctor enim. Fusce elementum fermentum nulla nec congue. Nulla facilisi.<br><br>",
-			"Praesent fermentum tristique quam, at faucibus est congue vel. Sed vel lacus euismod, vehicula eros vel, fringilla nulla. In hac habitasse platea dictumst. Integer ut eleifend eros. Cras vitae convallis ex, id tincidunt lorem. Aenean tristique odio a sapien vehicula volutpat. Fusce lobortis nisi id augue cursus, in rhoncus lacus tincidunt. Proin non malesuada sem. Sed eu quam non enim posuere gravida. Sed commodo mi et sapien eleifend, ac scelerisque magna molestie. Vivamus vel neque euismod, faucibus sapien vitae, tempus ligula. Aenean eu purus ac eros accumsan consectetur.<br><br>"
+			"<p class='parrafo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et semper ligula. Nullam lacinia libero quis felis molestie, nec faucibus libero consectetur. Quisque gravida urna vel nunc rutrum, vitae interdum turpis mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi accumsan lorem vitae bibendum dictum. Phasellus sollicitudin turpis et nisi sollicitudin, a dapibus dolor cursus. Ut et libero sed felis tincidunt efficitur. Phasellus commodo velit quis justo posuere, sed lacinia felis volutpat. Vivamus quis sodales metus. Sed luctus euismod tincidunt.<br><br></p>",
+			"<div class='subtitle'>Hola Mundo!!!</div>",
+			"<p class='parrafo'>Duis ac metus nec neque tincidunt vulputate. Donec eu ex nec ligula vulputate fermentum eget sit amet est. Vivamus at ante vel elit consectetur dapibus. Ut auctor lacus et diam laoreet, at tempor ipsum volutpat. Nullam ut nisi purus. Cras non bibendum arcu, at auctor enim. Fusce elementum fermentum nulla nec congue. Nulla facilisi.<br><br></p>",
+			"<div class='subtitle'>Probando subtitulos...</div>",
+			"<p class='parrafo'>Praesent fermentum tristique quam, at faucibus est congue vel. Sed vel lacus euismod, vehicula eros vel, fringilla nulla. In hac habitasse platea dictumst. Integer ut eleifend eros. Cras vitae convallis ex, id tincidunt lorem. Aenean tristique odio a sapien vehicula volutpat. Fusce lobortis nisi id augue cursus, in rhoncus lacus tincidunt. Proin non malesuada sem. Sed eu quam non enim posuere gravida. Sed commodo mi et sapien eleifend, ac scelerisque magna molestie. Vivamus vel neque euismod, faucibus sapien vitae, tempus ligula. Aenean eu purus ac eros accumsan consectetur.<br><br></p>"
+		]
+	},
+	"revolucionfrancesa":{
+		"id":1,
+		"autor":"Marco Patrico Debiasi",
+		"fecha":"16/04/2024",
+		"titulo":"Revolusion Francesa",
+		"texto":[
+			"<p class='parrafo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et semper ligula. Nullam lacinia libero quis felis molestie, nec faucibus libero consectetur. Quisque gravida urna vel nunc rutrum, vitae interdum turpis mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi accumsan lorem vitae bibendum dictum. Phasellus sollicitudin turpis et nisi sollicitudin, a dapibus dolor cursus. Ut et libero sed felis tincidunt efficitur. Phasellus commodo velit quis justo posuere, sed lacinia felis volutpat. Vivamus quis sodales metus. Sed luctus euismod tincidunt.<br><br></p>",
+			"<div class='subtitle'>Hola Mundo!!!</div>",
+			"<p class='parrafo'>Duis ac metus nec neque tincidunt vulputate. Donec eu ex nec ligula vulputate fermentum eget sit amet est. Vivamus at ante vel elit consectetur dapibus. Ut auctor lacus et diam laoreet, at tempor ipsum volutpat. Nullam ut nisi purus. Cras non bibendum arcu, at auctor enim. Fusce elementum fermentum nulla nec congue. Nulla facilisi.<br><br></p>",
+			"<div class='subtitle'>Probando subtitulos...</div>",
+			"<p class='parrafo'>Praesent fermentum tristique quam, at faucibus est congue vel. Sed vel lacus euismod, vehicula eros vel, fringilla nulla. In hac habitasse platea dictumst. Integer ut eleifend eros. Cras vitae convallis ex, id tincidunt lorem. Aenean tristique odio a sapien vehicula volutpat. Fusce lobortis nisi id augue cursus, in rhoncus lacus tincidunt. Proin non malesuada sem. Sed eu quam non enim posuere gravida. Sed commodo mi et sapien eleifend, ac scelerisque magna molestie. Vivamus vel neque euismod, faucibus sapien vitae, tempus ligula. Aenean eu purus ac eros accumsan consectetur.<br><br></p>"
+		]
+	},
+	"historiaargentina":{
+		"id":2,
+		"autor":"Marco Patrico Debiasi",
+		"fecha":"16/04/2024",
+		"titulo":"Historia Argentina",
+		"texto":[
+			"<p class='parrafo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et semper ligula. Nullam lacinia libero quis felis molestie, nec faucibus libero consectetur. Quisque gravida urna vel nunc rutrum, vitae interdum turpis mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi accumsan lorem vitae bibendum dictum. Phasellus sollicitudin turpis et nisi sollicitudin, a dapibus dolor cursus. Ut et libero sed felis tincidunt efficitur. Phasellus commodo velit quis justo posuere, sed lacinia felis volutpat. Vivamus quis sodales metus. Sed luctus euismod tincidunt.<br><br></p>",
+			"<div class='subtitle'>Hola Mundo!!!</div>",
+			"<p class='parrafo'>Duis ac metus nec neque tincidunt vulputate. Donec eu ex nec ligula vulputate fermentum eget sit amet est. Vivamus at ante vel elit consectetur dapibus. Ut auctor lacus et diam laoreet, at tempor ipsum volutpat. Nullam ut nisi purus. Cras non bibendum arcu, at auctor enim. Fusce elementum fermentum nulla nec congue. Nulla facilisi.<br><br></p>",
+			"<div class='subtitle'>Probando subtitulos...</div>",
+			"<p class='parrafo'>Praesent fermentum tristique quam, at faucibus est congue vel. Sed vel lacus euismod, vehicula eros vel, fringilla nulla. In hac habitasse platea dictumst. Integer ut eleifend eros. Cras vitae convallis ex, id tincidunt lorem. Aenean tristique odio a sapien vehicula volutpat. Fusce lobortis nisi id augue cursus, in rhoncus lacus tincidunt. Proin non malesuada sem. Sed eu quam non enim posuere gravida. Sed commodo mi et sapien eleifend, ac scelerisque magna molestie. Vivamus vel neque euismod, faucibus sapien vitae, tempus ligula. Aenean eu purus ac eros accumsan consectetur.<br><br></p>"
+		]
+	},
+	"revolucionrusa":{
+		"id":3,
+		"autor":"Marco Patrico Debiasi",
+		"fecha":"16/04/2024",
+		"titulo":"Revolucion rusa",
+		"texto":[
+			"<p class='parrafo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et semper ligula. Nullam lacinia libero quis felis molestie, nec faucibus libero consectetur. Quisque gravida urna vel nunc rutrum, vitae interdum turpis mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi accumsan lorem vitae bibendum dictum. Phasellus sollicitudin turpis et nisi sollicitudin, a dapibus dolor cursus. Ut et libero sed felis tincidunt efficitur. Phasellus commodo velit quis justo posuere, sed lacinia felis volutpat. Vivamus quis sodales metus. Sed luctus euismod tincidunt.<br><br></p>",
+			"<div class='subtitle'>Hola Mundo!!!</div>",
+			"<p class='parrafo'>Duis ac metus nec neque tincidunt vulputate. Donec eu ex nec ligula vulputate fermentum eget sit amet est. Vivamus at ante vel elit consectetur dapibus. Ut auctor lacus et diam laoreet, at tempor ipsum volutpat. Nullam ut nisi purus. Cras non bibendum arcu, at auctor enim. Fusce elementum fermentum nulla nec congue. Nulla facilisi.<br><br></p>",
+			"<div class='subtitle'>Probando subtitulos...</div>",
+			"<p class='parrafo'>Praesent fermentum tristique quam, at faucibus est congue vel. Sed vel lacus euismod, vehicula eros vel, fringilla nulla. In hac habitasse platea dictumst. Integer ut eleifend eros. Cras vitae convallis ex, id tincidunt lorem. Aenean tristique odio a sapien vehicula volutpat. Fusce lobortis nisi id augue cursus, in rhoncus lacus tincidunt. Proin non malesuada sem. Sed eu quam non enim posuere gravida. Sed commodo mi et sapien eleifend, ac scelerisque magna molestie. Vivamus vel neque euismod, faucibus sapien vitae, tempus ligula. Aenean eu purus ac eros accumsan consectetur.<br><br></p>"
 		]
 	}
 }
 `;
 
 
-export{articulo1};
+
+export{articulo};
 
--- js/main.js
+++ js/main.js
@@ -1,18 +1,57 @@
-import {articulo1} from './bbdd.js';
+import * as base from './bbdd.js';
 
-let data,texto;
+let data,texto,titulo;
+
+
+let menuFloats = document.getElementById("menufloat");
+let ro = JSON.parse(base.articulo);
+let info = Object.keys(ro);
+let articles = Object.values(ro);
+var BTNmenu = `<p id=${articles[0].id} class='tema'>${articles[0].titulo}</p>`;
+for (let i=1; i<info.length; i++){
+	BTNmenu += `<p id=${articles[i].id} class='tema'>${articles[i].titulo}</p>`;
+}
+menuFloats.innerHTML = `
+<div class="menu">
+	<h3>Art&iacute;culos</h3>
+	${BTNmenu}
+</div>
+`;
+
 
 let btn_inicio = document.getElementById("inicio");
 btn_inicio.addEventListener("click",inicio);
 
 let btn_art = document.getElementById("articulo");
-btn_art.addEventListener("click",art);
+btn_art.addEventListener("click",arti);
 
 let btn_con = document.getElementById("contacto");
 btn_con.addEventListener("click",con);
 
+
+// Cuerpo del articulo
 let text = document.getElementById("text");
+// Titulo del articulo
 let tittle = document.getElementById("tittle");
+
+//deteccion del temario
+let temas = [];
+for(let i=0; i<info.length; i++){
+	temas[i] = document.getElementById(`${articles[i].id}`);
+}
+
+//mecanicas del temario
+for(let i=0; i<info.length; i++){
+	temas[i].addEventListener("click",function(){
+		getArticles(i);
+		art();
+	});
+}
+
+
+
+
+
 
 function inicio()
 {
@@ -20,11 +59,25 @@ function inicio()
 	tittle.innerHTML = "Nombre del art&iacute;culo";
 }
 
+let menuFloat = false;
+function arti()
+{
+	if(menuFloat==false){
+		let menufloat = document.getElementById("menufloat");
+		menufloat.style.display = "block";
+		menuFloat = true;
+	}else{
+		let menufloat = document.getElementById("menufloat");
+		menufloat.style.display = "none";
+		menuFloat = false;
+	}
+}
+
 function art()
 {
-	getArticulo(articulo1);
 	text.innerHTML = texto;
-	tittle.innerHTML = "Revoluci&oacute;n francesa.";
+	tittle.innerHTML = titulo;
+	arti();
 }
 
 function con()
@@ -34,15 +87,17 @@ function con()
 }
 
 
-function getArticulo(art)
+function getArticles(i)
 {
-	data = JSON.parse(art);
-	texto = data.articulo.texto + "<br><br>";
-	texto += data.articulo.autor + "<br>" + data.articulo.fecha;
-	texto = "<p class='parrafo'>" + texto + "</p>";
+	data = JSON.parse(base.articulo);
+	let info = Object.keys(data);
+	let articles = Object.values(data);
+	
+	texto = articles[i].texto;
+	texto = texto.join("") + "<br><br>";
+	texto += articles[i].autor + "<br>" + articles[i].fecha;
+	titulo = articles[i].titulo;
 }
-
-
 
 
 
